b8cf04053dfb656076a0b1d8337ed8737da94492
Rafael <rafael.cavallari@hotmail.com>
2024-10-02 22:09:59 -0300

    First Commit

---
+++ canva_bot/README.md
@@ -0,0 +1,33 @@
+# Canva Bot
+> This script communicates with the Canva interface using Pyautogui.
+
+**Canva Bot** is an automated script that interacts with Canva's 
+interface to create and edit videos based on data provided in a 
+Google Sheets spreadsheet. By using libraries such as pyautogui 
+and pyperclip, the script simulates mouse movements and clicks, 
+as well as copying and pasting text, to automatically fill 
+specific templates in Canva.
+
+## Features
+* **Video Editing Automation:** 
+Fills in titles, subtitles, questions, and answer options in a 
+video template on Canva.
+* **Effects Control:** Activates or deactivates visual effects 
+based on the answers provided.
+* **Google Sheets Integration:** Imports data directly from a 
+Google Sheets spreadsheet to feed the video creation process.
+
+
+## Requirements
+Before running the script, make sure to have the following Python 
+packages installed:
+* `pandas`
+* `pyautogui`
+* `pyperclip`
+
+You can install them using `pip`:
+
+`pip install pandas pyautogui pyperclip`
+
+
+
---
+++ canva_bot/main.py
@@ -0,0 +1,213 @@
+import pandas as pd
+import pyautogui
+import time
+import pyperclip
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+
+def create_video(df):
+    def switch_page():
+        time.sleep(1)
+        pyautogui.moveTo(800, 450)
+        pyautogui.keyDown('alt')  # Press and hold 'Alt'
+        pyautogui.press('tab')  # Press and release 'tab'
+        pyautogui.keyUp('alt')  # Release 'Alt'
+        time.sleep(0.1)
+
+    def effect_control(x_coord, y_coord, op, ans):
+        def effect():
+            pyautogui.moveTo(420, 800, duration=0.5)
+            time.sleep(0.5)
+            pyautogui.mouseDown()
+            time.sleep(0.5)
+            pyautogui.mouseUp()
+
+            time.sleep(0.5)
+            pyautogui.moveTo(350, 815, duration=0.5)
+            pyautogui.click()  # Remove Effect Button
+            time.sleep(0.5)
+            pyautogui.moveTo(350, 630, duration=0.5)
+            pyautogui.click()  # Effect Button
+
+        print(f"op: {op} | ans: {ans}")
+        if op == ans:
+            print('Ativa o efeito')
+            pyautogui.moveTo(1200, 205, duration=0.5)
+            pyautogui.click()  # Effect
+            time.sleep(0.5)
+            effect()
+            time.sleep(0.5)
+            pyautogui.moveTo(x_coord, y_coord + 15, duration=0.5)
+            pyautogui.click()  # Text
+            time.sleep(0.5)
+            effect()
+            time.sleep(0.5)
+            pyautogui.moveTo(620, 205, duration=0.5)
+            pyautogui.click()  # Colors
+            time.sleep(0.5)
+            pyautogui.moveTo(110, 415, duration=0.5)
+            pyautogui.click()  # Effect Color
+        else:
+            print('Desativa o efeito')
+            pyautogui.moveTo(1200, 205, duration=0.5)
+            pyautogui.click()  # Effects
+            time.sleep(0.5)
+            effect()
+            time.sleep(0.5)
+            pyautogui.moveTo(350, 815, duration=0.5)
+            pyautogui.click()  # Remove Effect Button
+            time.sleep(0.5)
+            pyautogui.moveTo(x_coord, y_coord + 15, duration=0.5)
+            pyautogui.click()  # Text Card
+            time.sleep(0.5)
+            effect()
+            time.sleep(0.5)
+            pyautogui.moveTo(350, 815, duration=0.5)
+            pyautogui.click()  # Remove Effect Button
+            time.sleep(0.5)
+            pyautogui.moveTo(620, 205, duration=0.5)
+            pyautogui.click()  # Colors
+            time.sleep(0.5)
+            pyautogui.moveTo(165, 415, duration=0.5)
+            pyautogui.click()  # No Effect Color
+
+        time.sleep(0.5)
+
+    def edit_page(data, page):  # Beware the Zoom
+        # ------------------- TITLE
+        pyautogui.moveTo(860, 285, duration=0.5)
+        pyperclip.copy(data['title'])  # Copy the text I nedd
+        pyautogui.doubleClick()
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'a')  # select all
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'v')  # paste
+        time.sleep(0.5)
+        pyautogui.click(500, 500)  # Exit Text
+        time.sleep(0.5)
+
+        # ------------------- SUBTITLE
+        pyautogui.moveTo(820, 315, duration=0.5)
+        pyperclip.copy(data['subtitle'])
+        pyautogui.doubleClick()
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(0.5)
+        pyautogui.click(500, 500)
+        time.sleep(0.5)
+
+        # ------------------- QUESTION
+        pyautogui.moveTo(840, 420, duration=0.5)
+        pyperclip.copy(data['question'])
+        pyautogui.doubleClick()
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(0.5)
+        pyautogui.click(500, 500)
+        time.sleep(0.5)
+
+        # ------------------- OP1
+        pyautogui.moveTo(790, 530, duration=0.5)
+        pyperclip.copy('A) ' + data['op1'])
+        pyautogui.doubleClick()
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(0.5)
+
+        if not page % 2 == 0:  # If the page is odd
+            effect_control(790, 530, 'A', data['answer'])
+
+        time.sleep(0.5)
+        pyautogui.click(500, 500)
+        time.sleep(0.5)
+
+        # ------------------- OP2
+        pyautogui.moveTo(790, 575, duration=0.5)
+        pyperclip.copy('B) ' + data['op2'])
+        pyautogui.doubleClick()
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(0.5)
+
+        if not page % 2 == 0:  # If the page is odd
+            effect_control(790, 575, 'B', data['answer'])
+
+        time.sleep(0.5)
+        pyautogui.click(500, 500)
+        time.sleep(0.5)
+
+        # ------------------- OP3
+        pyautogui.moveTo(790, 615, duration=0.5)
+        pyperclip.copy('C) ' + data['op3'])
+        pyautogui.doubleClick()
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(0.5)
+
+        if not page % 2 == 0:  # If the page is odd
+            effect_control(790, 615, 'C', data['answer'])
+
+        time.sleep(0.5)
+        pyautogui.click(500, 500)
+        time.sleep(0.5)
+
+    screen_width, screen_height = pyautogui.size()  # Screen Size
+    print(f"Largura: {screen_width}, Altura: {screen_height}")
+
+    # ------------------- BROWSER
+
+    switch_page()
+
+    # ------------------- OTHER PAGES
+
+    for i in range(10):
+        if i == 0:  # Only first loop
+            pyautogui.moveTo(250, 750, duration=0.5)
+            pyautogui.click()
+        else:
+            pyautogui.moveTo(360, 500, duration=0.5)  # Blank space
+            pyautogui.click()
+            time.sleep(0.5)
+            pyautogui.scroll(-100)
+
+        payload = {
+            'title': df['title'].iloc[i],
+            'subtitle': df['subtitle'].iloc[i],
+            'question': df['question'].iloc[i],
+            'op1': df['op1'].iloc[i],
+            'op2': df['op2'].iloc[i],
+            'op3': df['op3'].iloc[i],
+            'answer': df['answer'].iloc[i]
+        }
+
+        time.sleep(0.5)
+        edit_page(payload, i)
+
+
+def main():
+
+    # Get Dataframe
+    sheet_id = '1ougkayhtVRRCc88jWGRMFdTBBM7AafbSEZsdqCRVwJE'
+    df_original = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    duplicated_df = df_original.copy()
+    df = pd.concat([df_original, duplicated_df], ignore_index=True).sort_values(by='page')
+
+    print(df)
+    create_video(df)
+
+
+main()
