8d381de60b6718edb06ad36e50d4e2f6ffc2fdf8
Rafael <rafael.cavallari@hotmail.com>
2024-11-06 01:05:39 -0300

    Now, I don't have to enter the name of the video. This script can list all the videos into a path and generate the audios

--- tiktok_videos/audio_generating.py
+++ tiktok_videos/audio_generating.py
@@ -1,11 +1,16 @@
+import time
+
 from dotenv import load_dotenv
 import os
 from elevenlabs import save
 from elevenlabs.client import ElevenLabs
 import pandas as pd
+import sys
 
-VIDEO_NAME = '0001-conhecimentos-gerais-medio'
-VIDEO_PATH = r'C:\Users\Rafae\Videos\TikTok\Audios'
+HOME_DIR = os.path.expanduser("~")
+VIDEOS_INCOMPLETOS_DIR = os.path.join(HOME_DIR, "Videos", "TikTok", "Incompletos")
+VIDEOS_DIR = os.path.join(HOME_DIR, "Videos", "TikTok")
+AUDIO_PATH = os.path.join(HOME_DIR, "Videos", "TikTok", "Audios")
 
 load_dotenv()
 
@@ -14,7 +19,16 @@ client = ElevenLabs(api_key=os.getenv("ELEVEN_API_KEY"))
 user = client.user.get()
 
 
-def generate_audio(txt_to_speech, q):
+def generate_audio(txt_to_speech, q, video_name):
+
+    # Here we get the audio's name
+    audio_name = video_name + f'-q{q}.mp3'
+
+    # Verify if the audios already exist. If so, return
+    audios = os.listdir(AUDIO_PATH)
+    if audio_name in audios:
+        print(f" | {audio_name} already exists into {AUDIO_PATH}")
+        return
 
     audio = client.generate(
       text=txt_to_speech,
@@ -22,23 +36,61 @@ def generate_audio(txt_to_speech, q):
       model="eleven_multilingual_v2"
     )
 
-    save(audio=audio, filename=os.path.join(VIDEO_PATH, VIDEO_NAME + f'-q{q}.mp3'))
+    save(audio=audio, filename=os.path.join(AUDIO_PATH, audio_name))
+
+    print(f" | Audio generated successfully!")
 
 
 def main():
-    print("Credit Quota Total:", user.subscription.character_limit)
+    # List all the videos not completed
+    files = os.listdir(VIDEOS_INCOMPLETOS_DIR)
 
+    # If there is no video, exit. Otherwise, keep going
+    if not files:
+        print(f"0 videos found in the path {VIDEOS_INCOMPLETOS_DIR}")
+        sys.exit()
+    else:
+        print(f"{len(files)} video(s) found in the path {VIDEOS_INCOMPLETOS_DIR}")
+
+    # Generate df
     sheet_id = '1ougkayhtVRRCc88jWGRMFdTBBM7AafbSEZsdqCRVwJE'
-    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    gid = '988783826'
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
+    df = pd.read_csv(url)
 
-    for i in range(len(df)):
-        question = df['question'].iloc[i]
-        print(f"Pergunta {i+1} de {len(df)}: {question}", end='')
-        generate_audio(question, i+1)
-        print(f" | Áudio gerado com sucesso!")
+    # Read each file inside directory
+    for file in files:
+        print(f'\n### {file}')
 
-    restantes = user.subscription.character_limit - user.subscription.character_count
-    print("Credit Quota Remaining:", restantes)
+        # Split file and extension
+        file_name, file_ext = os.path.splitext(file)
+
+        # Create a copy of df. Filtering just the questions of the video
+        df_filtered = df.copy()
+        df_filtered = df_filtered[df_filtered['video_name'] == file_name]
+
+        # Verify the quota
+        df_temp = df_filtered.copy()  # Had to generate another df
+        quota_nedded = df_temp['question_len'].astype(int).sum()
+        quota_total = user.subscription.character_limit
+        print(f"Credit Quota Need: {quota_nedded} | You Have: {quota_total}", end='')
+        if quota_total >= quota_nedded:
+            print(" | You have enough :)")
+        else:
+            print(" | You don't have enough :(")
+            sys.exit()
+
+        # Generate the audios
+        for i in range(len(df_filtered)):
+            question = df_filtered['question'].iloc[i]
+            print(f"Question {i + 1} of {len(df_filtered)}: {question}", end='')
+            generate_audio(question, i + 1, file_name)
+
+
+        # restantes = user.subscription.character_limit - user.subscription.character_count
+        # print("Credit Quota Remaining:", restantes)
+
+
 
 
 main()
